0617a7034d6f2c7485e030bf5d82f5c93c85a993
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-04-14 18:08:11 -0400

    fixed the API call. Included variables for each element of the API call.

--- script.js
+++ script.js
@@ -8,20 +8,30 @@ $("button").on("click", function () {
     var userChoice = searchButton.val();
     pullApi();
     console.log(userChoice);
-    console.log(pullApi);
+    // console.log(pullApi);
 
     // run function to pull API info. 
     function pullApi() {
 
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userChoice + "i&appid=62fbd3039df5554b8330852eff63de44"
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userChoice + "&appid=62fbd3039df5554b8330852eff63de44"
         $.ajax({
             url: queryURL,
             method: "GET"
         })
             // We store all of the retrieved data inside of an object called "response"
             .then(function (response) {
-                // var results = response.response.docs
                 console.log(response);
+                var searchName = response.name;
+                // console.log(searchName);
+                var searchTemp = response.main.temp;
+                // console.log(searchTemp);
+                var searchTemp = response.main.feels_like;
+                var searchTemp = response.main.temp_min;
+                var searchTemp = response.main.temp_max;
+                var searchTemp = response.main.pressure;
+                var searchTemp = response.main.humidity;
+
+
                 // console.log(results[0].snippet);
                 // for (var i = 0; i < results.length; i++)
                 //     var firstSnippet = results[i].snippet;
@@ -29,6 +39,4 @@ $("button").on("click", function () {
             })
     }
 })
-
-// capture the user input by the id"fname"
 
