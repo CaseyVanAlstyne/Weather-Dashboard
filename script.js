4711d9be3c8f2b9e009266a13cbc5503a7df31d6
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-04-15 09:56:52 -0400

    added second function for API. This API focuses on the five day forecast.

--- script.js
+++ script.js
@@ -6,37 +6,57 @@ var searchButton = $("#fname");
 $("button").on("click", function () {
     event.preventDefault();
     var userChoice = searchButton.val();
-    pullApi();
+    pullApiWeather(userChoice);
+    pullApiForecast(userChoice);
+    // pullApiUV();
     console.log(userChoice);
     // console.log(pullApi);
-
-    // run function to pull API info. 
-    function pullApi() {
-
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userChoice + "&appid=62fbd3039df5554b8330852eff63de44"
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-            // We store all of the retrieved data inside of an object called "response"
-            .then(function (response) {
-                console.log(response);
-                var searchName = response.name;
-                // console.log(searchName);
-                var searchTemp = response.main.temp;
-                // console.log(searchTemp);
-                var searchTemp = response.main.feels_like;
-                var searchTemp = response.main.temp_min;
-                var searchTemp = response.main.temp_max;
-                var searchTemp = response.main.pressure;
-                var searchTemp = response.main.humidity;
-
-
-                // console.log(results[0].snippet);
-                // for (var i = 0; i < results.length; i++)
-                //     var firstSnippet = results[i].snippet;
-                // $("#resultSnippet").append(firstSnippet);
-            })
-    }
 })
 
+// run function to pull API info for current weather data. 
+function pullApiWeather(userChoice) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userChoice + "&appid=62fbd3039df5554b8330852eff63de44";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        // We store all of the retrieved data inside of an object called "response"
+        .then(function (response) {
+            console.log("current stuff", response);
+            var searchName = response.name;
+            // console.log(searchName);
+            var searchTemp = response.main.temp;
+            // console.log(searchTemp);
+            var searchFeelsLike = response.main.feels_like;
+            var searchTempMin = response.main.temp_min;
+            var searchTempMax = response.main.temp_max;
+            var searchPressure = response.main.pressure;
+            var searchHumidity = response.main.humidity;
+        })
+}
+
+// run function to pull API info for 5 day/3 hour forecast. 
+function pullApiForecast(userChoice) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userChoice + "&appid=62fbd3039df5554b8330852eff63de44";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        // We store all of the retrieved data inside of an object called "response"
+        .then(function (response) {
+            // console.log("five day", response);
+            // console.log(response.list[7].dt_txt);
+            // console.log(response.list[15].dt_txt);
+            // console.log(response.list[23].dt_txt);
+            // console.log(response.list[31].dt_txt);
+            // console.log(response.list[39].dt_txt);
+
+            for (var i = 7; i < response.list.length; i += 8) {
+                console.log(i, response.list[i]);
+                response.list[i];
+            }
+        })
+}
+
+// Need a function for UV index API. There is a little trick - WATCH OUT!
+// look into template strings!
