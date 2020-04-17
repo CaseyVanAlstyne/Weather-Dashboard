f54dfe38e92e8b8f3a3fe0f2cd4390957bf882e7
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-04-16 22:35:27 -0400

    got the five day forecast to display on the page. Need to figure out the weather icon. Need to stuff into different vertical cards that actually look nice. Also need local storage on click for the user choices.

--- script.js
+++ script.js
@@ -32,11 +32,8 @@ function pullApiWeather(userChoice) {
             var searchHumidity = response.main.humidity;
             var searchLonLat = response.coord;
             pullApiUV(searchLonLat);
-            console.log(response.coord)
-
+            // console.log(response.coord)
             var weatherDiv = $("<div>");
-
-
             var mainContainer = $("<div>").addClass("jumbotron")
             var p0 = $("<div>").addClass("card").text("This is what you searched for: " + searchName);
             var p = $("<div>").addClass("card").text("Current Temperature: " + searchTemp);
@@ -45,12 +42,9 @@ function pullApiWeather(userChoice) {
             var p3 = $("<div>").addClass("card").text("The maximum temperature is " + searchTempMax);
             var p4 = $("<div>").addClass("card").text("The humidity level is " + searchHumidity);
             var p5 = $("<div>").addClass("card").text("This is where you currently are. I need to access this information in another function/api call, but can't figure it out. " + JSON.stringify(searchLonLat));
-
             mainContainer.append(p0, p, p1, p2, p3, p4, p5);
             weatherDiv.append(mainContainer);
-
-
-            $("#resultSnippet").prepend(weatherDiv);
+            $("#resultWeatherSnippet").prepend(weatherDiv);
         })
 }
 
@@ -72,6 +66,16 @@ function pullApiForecast(userChoice) {
                 console.log(i, response.list[i]);
                 response.list[i];
             }
+            var weatherDiv = $("<div>");
+            var mainContainer = $("<div>").addClass("jumbotron")
+            var day1 = $("<div>").addClass("card").text("Here is the weather icon: " + response.list[7].weather[0].icon);
+            var day2 = $("<div>").addClass("card").text("The average temperature for day 2 is: " + response.list[15].main.temp);
+            var day3 = $("<div>").addClass("card").text("The wind speed for day 3 is: " + response.list[23].wind.speed);
+            var day4 = $("<div>").addClass("card").text("Temps feels like: " + response.list[31].main.feels_like);
+            var day5 = $("<div>").addClass("card").text("Max temp for day 5: " + response.list[39].main.temp_max);
+            mainContainer.append(day1, day2, day3, day4, day5);
+            weatherDiv.append(mainContainer);
+            $("#resultForecastSnippet").prepend(weatherDiv);
         })
 }
 
@@ -95,6 +99,5 @@ function renderPreviousSearch(userChoice) {
     var previousSearchButton = $("<button>").text(userChoice);
     $("#repeatButton").prepend(previousSearchButton);
     // pullApiWeather(userChoice);
-
     // Create onclick for #repeatButton that will run the pullApiWeather(userChoice) ************************* Expect this not to work
 }
